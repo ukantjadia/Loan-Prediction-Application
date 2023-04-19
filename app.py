83ef08c0cfb078f1b06d4300aefd16f06012b4fa
UkantJadia <ukantjadia0120@gmail.com>
2023-04-19 13:24:57 +0530

    update

--- app.py
+++ app.py
@@ -22,49 +22,48 @@ def main():
     """
     st.markdown(html_temp, unsafe_allow_html=True)
 
-    col1,col2  = st.columns([2,2])
-    
-    with col1: 
-        st.sidebar.write(""" ## How does it work ❓
-                         
+    st.sidebar.write(""" ## How does it work ❓
 The loan approval prediction model uses your credit score, income, education, loan amount and other various factor  to determine whether your loan will be approved or not.  It outputs a "yes" or "no" decision.
                 """)
-        with st.sidebar.expander(" ℹ️ Information", expanded=True):
-            st.write("""
-                    Loan Prediction is very helpful for employee of banks 
-                    as well as for the applicant also.
-                    Dream housing Finance Company 
-                    deals in all loans. They have presence across all urban, 
-                    semi urban and rural areas. Customer first apply for loan 
-                    after that company or bank validates the customer 
-                    eligibility for loan. Company or bank wants to automate 
-                    the loan eligibility process (real time) based on customer 
-                    details provided while filling application form. These 
-                    details are Gender, Marital Status, Education, Number of 
-                    Dependents, Income, Loan Amount, Credit History and 
-                    other
-            """)
+    
 
-            gender = st.radio("Gender", ['Male','Female'])
-            married = st.radio("Married", ['Yes','No'])
-            dependents = st.radio("Dependents", ['0','1','2','3'])
+    with st.sidebar.expander(" ℹ️ Information", expanded=True):
+        st.write("""
+                Loan Prediction is very helpful for employee of banks 
+                as well as for the applicant also.
+                Dream housing Finance Company 
+                deals in all loans. They have presence across all urban, 
+                semi urban and rural areas. Customer first apply for loan 
+                after that company or bank validates the customer 
+                eligibility for loan. Company or bank wants to automate 
+                the loan eligibility process (real time) based on customer 
+                details provided while filling application form. These 
+                details are Gender, Marital Status, Education, Number of 
+                Dependents, Income, Loan Amount, Credit History and 
+                other
+        """)
 
 
-# attributes are 
-    with col2:
-        st.subheader(" Find out the most suitable crop to grow in your farm 👨‍🌾")
-        col3,col4  = st.columns([2,2])
-        with col3:
-            applicantIncome = st.number_input("Applicant Income", 150.0,100000.0)
-            coApplicantIncome = st.number_input("Co Applicant Income", 0.0,50000.0)
-            loanAmount = st.number_input("Loan Amount", 10.0,1000.0)
+
+    with st.form("my_form"):
+        col4,col6 = st.columns([2,2])
         with col4:
             education = st.radio("Education", ['Graduate','Not Graduate'])
-            employed = st.radio("Self Employed", ['Yes','No'])
-            propertyArea = st.radio("Property Area", ['Semiurban','Urban'])
+            married = st.radio("Married", ['Yes','No'])
             loanAmountTerm = st.number_input("Loan Amount Term", 10.0,600.0)
+            applicantIncome = st.number_input("Applicant Income", 150.0,100000.0)
+            propertyArea = st.radio("Property Area", ['Ruler','Semiurban','Urban'])
+        with col6:
+            employed = st.radio("Self Employed", ['Yes','No'])
+            gender = st.radio("Gender", ['Male','Female'])
+            coApplicantIncome = st.number_input("Co Applicant Income", 0.0,50000.0)
+            loanAmount = st.number_input("Loan Amount", 10.0,1000.0)
             creditHistory = str(st.slider("Credit History",1,3000))
+            dependents = st.slider("Dependents", 0,4)
 
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Submit")
+        if submitted:
             num_data = [applicantIncome, coApplicantIncome, loanAmount, loanAmountTerm]
             cat_data = [gender, married, dependents, education, employed,creditHistory, propertyArea]
             test=[]
@@ -72,8 +71,6 @@ The loan approval prediction model uses your credit score, income, education, lo
             feature_list = num_data + test
             single_pred = np.array(feature_list).reshape(1,-1)
         
-        
-        if st.button('Predict'):
             loaded_model = load_model('model.sav')
             prediction = loaded_model.predict(single_pred)
             col1.write('''
@@ -83,11 +80,10 @@ The loan approval prediction model uses your credit score, income, education, lo
                 st.write("Yes")
             else: 
                 st.write("No")
-            # st.write(prediction[0])
-            col1.success(f"{prediction} are recommended by the A.I for your farm.")
+
+        #     col1.success(f"{prediction} are recommended by the A.I for your farm.")
       #code for html ☘️ 🌾 🌳 👨‍🌾  🍃
 
-    # st.warning("Note: This A.I application is for educational/demo purposes only and cannot be relied upon. Check the source code [here](https://github.com/gabbygab1233/Crop-Recommendation)")
     hide_menu_style = """
     <style>
     #MainMenu {visibility: hidden;}
